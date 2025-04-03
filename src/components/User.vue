05cda6ee2d66375bca88baae419c2eb52f4ad07b
AnasMaruf <anasmaruf1101@gmail.com>
2025-04-03 10:08:13 +0700

    Programmatic Navigation

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,8 +1,19 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 <template>
   <h1>User</h1>
-  <div>Put header here</div>
+  <ul>
+    <li><RouterLink to="/users/profile">Profile</RouterLink></li>
+    <li><RouterLink to="/users/order">Order</RouterLink></li>
+    <li><RouterLink to="/users/wishlist">Wishlist</RouterLink></li>
+  </ul>
   <RouterView />
   <div>Put footer here</div>
 </template>
-<style></style>
+<style scoped>
+.router-link-exact-active {
+  color: red;
+  font-weight: bold;
+}
+</style>
